097556eb9f5215f31b93bcae7e585c68e9f0a40d
Ogabek <ogabekjan07@gmail.com>
2022-06-13 15:47:35 +0500

    added bg image and title

--- TicTacToe/MainViewController.swift
+++ TicTacToe/MainViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    lazy var titleLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "Tic Tac Toe"
+        view.textColor = .yellow
+        view.textAlignment = .center
+        view.numberOfLines = 0
+        view.font = .systemFont(ofSize: 60.0, weight: .black)
+        
+        return view
+    }()
+    
+    lazy var bgImageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = #imageLiteral(resourceName: "ic_main_bg-1")
+        view.contentMode = .scaleAspectFill
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = .white
         
+        view.addSubview(bgImageView)
+        view.addSubview(titleLabel)
+        
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant:  40.0).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20.0).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20.0).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        bgImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        bgImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        bgImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        bgImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
 
 
