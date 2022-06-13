5f4e43d52401196002f7d38a31141b00eb8e9a2c
Ogabek <ogabekjan07@gmail.com>
2022-06-13 13:15:17 +0500

    setup programmatically making interface

---
+++ TicTacToe/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TicTacToe
+//
+//  Created by Ogabek Bakhodirov on 13/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+}
+
---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Ogabek Bakhodirov on 13/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        
+    }
+
+
+}
+
